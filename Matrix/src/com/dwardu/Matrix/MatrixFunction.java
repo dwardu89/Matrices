829aaa5ced5eedfcfad44d62976a033847dd9e47
dwardu89 <eddvella89@gmail.com>
2014-02-03 12:41:32 +0000

    Update MatrixFunction.java

--- Matrix/src/com/dwardu/Matrix/MatrixFunction.java
+++ Matrix/src/com/dwardu/Matrix/MatrixFunction.java
@@ -114,6 +114,10 @@ public class MatrixFunction {
 
 	/*
 	 * Creates a diagonal matrix
+	 * 
+	 * @param a[] the list of integers to place in the diagonal matrix
+	 * 
+	 * @return the diagonal matrix
 	 */
 	public static Matrix createDiagonal(int a[]) {
 		Matrix diagonal = new Matrix(a.length, a.length);
@@ -122,6 +126,25 @@ public class MatrixFunction {
 		return diagonal;
 	}
 
+	/*
+	 * Creates a diagonal matrix
+	 * 
+	 * @param a[] the list of integers to place in the diagonal matrix
+	 * 
+	 * @return the diagonal matrix
+	 */
+	public static Matrix createDiagonal(double a[]) {
+		Matrix diagonal = new Matrix(a.length, a.length);
+		for (int n = 0; n < a.length; n++)
+			diagonal.setValueAt(n, n, a[n]);
+		return diagonal;
+	}
+
+	/*
+	 * Checks if the matrix in question is symmetric
+	 * 
+	 * @return true if matrix is symmetric, false if matrix is not symmetric.
+	 */
 	public static boolean isSymmetrixMatrix(Matrix matrix) {
 		if (matrix.getRowSize() == matrix.getColumnSize()) {
 			int n = matrix.getRowSize();
@@ -144,42 +167,41 @@ public class MatrixFunction {
 	 * @return symmetricMatrix The symmetric matrix, returns null if the matrix
 	 * is not a square matrix.
 	 */
-	public static Matrix createSymmetricUpper(Matrix matrix) throws CloneNotSupportedException {
+	public static Matrix createSymmetricUpper(Matrix matrix)
+			throws CloneNotSupportedException {
 		if (matrix.getRowSize() == matrix.getColumnSize()) {
-			Matrix symmetricMatrix  = (Matrix)matrix.clone();
+			Matrix symmetricMatrix = (Matrix) matrix.clone();
 			int n = matrix.getRowSize();
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < n; j++) {
 					if (i > j)
-						symmetricMatrix.setValueAt(i, j, matrix.getValueAt(j, i));
+						symmetricMatrix.setValueAt(i, j,
+								matrix.getValueAt(j, i));
 				}
-			} 
+			}
 			return symmetricMatrix;
 		} else
 			return null;
 	}
 
-	 public static Matrix createSymmetricLowerMatrix(Matrix matrix) throws CloneNotSupportedException
-     {
+	public static Matrix createSymmetricLowerMatrix(Matrix matrix)
+			throws CloneNotSupportedException {
 
-         if (matrix.getRowSize() == matrix.getColumnSize())
-         {
-             Matrix symmetricMatrix = (Matrix)matrix.clone();
-             int n = matrix.getRowSize();
-             for (int i = 0; i < n; i++)
-             {
-                 for (int j = 0; j < n; j++)
-                 {
-                     if (i < j)
-                         matrix.setValueAt(i, j, matrix.getValueAt(j, i));
-                 }
-             }
-             symmetricMatrix = matrix;
-             return symmetricMatrix;
-         }
-         else return null;
-     }
-	
+		if (matrix.getRowSize() == matrix.getColumnSize()) {
+			Matrix symmetricMatrix = (Matrix) matrix.clone();
+			int n = matrix.getRowSize();
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (i < j)
+						matrix.setValueAt(i, j, matrix.getValueAt(j, i));
+				}
+			}
+			symmetricMatrix = matrix;
+			return symmetricMatrix;
+		} else
+			return null;
+	}
+
 	/*
 	 * Creates a matrix with using the lower-triangular matrix
 	 * 
@@ -188,9 +210,10 @@ public class MatrixFunction {
 	 * @return symmetricMatrix The symmetric matrix, returns null if the matrix
 	 * is not a square matrix.
 	 */
-	public static Matrix createLowerTriangular(Matrix matrix) throws CloneNotSupportedException {
-		if (matrix.getRowSize() == matrix.getColumnSize()) {
-			Matrix symmetricMatrix  = (Matrix) matrix.clone();
+	public static Matrix createLowerTriangular(Matrix matrix)
+			throws CloneNotSupportedException {
+		if (matrix.isSquare() && matrix.getSize() == 2) {
+			Matrix symmetricMatrix = (Matrix) matrix.clone();
 			for (int i = 0; i < matrix.getRowSize(); i++) {
 				for (int j = 0; j < matrix.getColumnSize(); j++) {
 					if (i < j) {
@@ -202,29 +225,74 @@ public class MatrixFunction {
 		}
 		return null;
 	}
-	
+
 	/*
 	 * Gets the determinant of a matrix
 	 * 
 	 * @return determinant
 	 */
-	  public static double getDeterminant(Matrix a)
-      {
-          if (a.getRowSize() == 2 && a.getColumnSize() == 2)
-          {
-              return getTwoByTwoDeterminant(a);
-          }
-          //TODO: Cater for determinants of matrices that are higher than 2x2 square matrices
-          else return 0;
-      }
-	  
-	  /*
-	   * Gets the detereminant of a 2x2 matrix
-	   * 
-	   * @return determinant
-	   */
-    private static double getTwoByTwoDeterminant(Matrix a)
-    {
-        return a.getValueAt(0,0)*a.getValueAt(1,1) - a.getValueAt(0,1)*a.getValueAt(1,0);
-    }
+	public static double getDeterminant(Matrix matrix) {
+		if (matrix.getSize() == 1)
+			return matrix.getValueAt(0, 0);
+		if (matrix.getRowSize() == 2 && matrix.getColumnSize() == 2) {
+			return getTwoByTwoDeterminant(matrix);
+		} else {
+			double sum = 0.0d;
+
+			for (int i = 0; i < matrix.getColumnSize(); i++) {
+				sum += getSign(i) * matrix.getValueAt(0, i)
+						* getDeterminant(createSubMatrix(matrix, i, 0));
+			}
+			return sum;
+		}
+	}
+
+	/*
+	 * Creates a sub matrix which is used in conjunction with the getDeterminant
+	 * method
+	 * 
+	 * @return a smaller matrix
+	 */
+	private static Matrix createSubMatrix(Matrix matrix, int exclude_column,
+			int exclude_row) {
+
+		Matrix newMatrix = new Matrix(matrix.getColumnSize() - 1,
+				matrix.getRowSize() - 1);
+		int nCol = -1;
+		for (int i = 0; i < matrix.getColumnSize(); i++) {
+			if (i == exclude_column)
+				continue;
+			nCol++;
+			int nRow = -1;
+			for (int j = 0; j < matrix.getRowSize(); j++) {
+				newMatrix.setValueAt(++nRow, nCol, matrix.getValueAt(j, i));
+			}
+		}
+		return newMatrix;
+	}
+
+	/*
+	 * Will get the sign of a matrix, which is used for the determinant
+	 * if the number is even, then the sign returned is positive, if no then its negative.
+	 * 
+	 * @param i index
+	 * 
+	 * @return 1 if positive, -1 if negative
+	 */
+	private static int getSign(int i) {
+		if (i % 2 == 0)
+			return 1;
+		else
+			return -1;
+	}
+
+	/*
+	 * Gets the determinant of a 2x2 matrix
+	 * 
+	 * @return determinant
+	 */
+	private static double getTwoByTwoDeterminant(Matrix a) {
+		return a.getValueAt(0, 0) * a.getValueAt(1, 1) - a.getValueAt(0, 1)
+				* a.getValueAt(1, 0);
+	} 
 }
